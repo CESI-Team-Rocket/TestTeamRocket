57c66847554b5f95dfad5cafc94e151e7e1736de
Simon NNORUKA <simon.nnoruka@viacesi.fr>
2020-11-16 14:02:31 +0100

    chelou

---
+++ TestGitCommun/TestGitCommun/TestGitCommun.cpp
@@ -0,0 +1,11 @@
+// TestGitCommun.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello chelou wzezetar 3!\n";
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
